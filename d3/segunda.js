30aefc6cf38643cf43ddf5e74e5aaf62752dc194
mponcec <37454237+mponcec@users.noreply.github.com>
2018-04-20 18:13:54 -0300

    Add files via upload
    Segunda evaluación

---
+++ d3/segunda.js
@@ -0,0 +1,11 @@
+//aquí debes trabajar con datos de un JSON
+var w = 450;
+var h = 450;
+d3.select("https://chilecube.datawheel.us//cubes/education_enrollment").then(function(data){
+    
+    
+}) 
+	.append("svg")
+	.attr("width", 500)
+	.attr("height", 700 )
+	.style("background","#FFFFFF");
